27b5003748dfb74966e1494b7cf2b580c3cf3f35
augisticz <augisticz@gmail.com>
2016-06-09 12:47:39 +0700

    Fixed

--- import/pages/Feed.jsx
+++ import/pages/Feed.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Components
 import PostItems from '../ui/components/PostItem.jsx';
+import PostBox from '../ui/components/PostBox.jsx';
 
 export default class Feed extends React.Component {
   render() {
     return(
       <div className="row">
+        <PostBox />
         <div className="col l12 feed">
           <PostItems postType="image"/>
           <PostItems postType="embed"/>
---
+++ import/ui/components/PostBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class PostBox extends React.Component {
+  render() {
+    return(
+      <div className="post-box">
+        
+      </div>
+    )
+  }
+}
